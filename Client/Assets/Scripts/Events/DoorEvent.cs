c2d256da893df3aa28ec411c3bbfe86d588499f9
Josema14 <josemaespin14@gmail.com>
2023-04-03 09:29:09 +0200

    Password rooms prefab

--- Client/Assets/Scripts/Events/DoorEvent.cs
+++ Client/Assets/Scripts/Events/DoorEvent.cs
@@ -8,19 +8,46 @@ public class DoorEvent : MonoBehaviour, IMetaEvent
 {
 
     public string map;
+
+    
+   [SerializeField] private bool isPublic = true;
+   [SerializeField] private string password;
+    public string lastPassword = "";
     GameManager gameManager;
 
     public void activate(bool host)
     {
 
-        gameManager = GameManager.FindInstance();
+        //If the room is public, we change the map
+        if (isPublic)
+        {
 
-        //Activate the loading UI
+            gameManager = GameManager.FindInstance();
 
-        //GameObject Loading = GameObject.Find("PlayerUIPrefab").transform.GetChild(4).gameObject;
-        //Loading.SetActive(true);
+            //Activate the loading UI
 
-        gameManager.ChangeMap(map);
+            //GameObject Loading = GameObject.Find("PlayerUIPrefab").transform.GetChild(4).gameObject;
+            //Loading.SetActive(true);
+
+            gameManager.ChangeMap(map);
+        }
+
+        //If the room is not public, we will ask the password
+
+        else
+        {
+            //Password
+            if (!lastPassword.Equals(password))
+            {
+                //We open the UI
+            }
+
+            else
+            {
+                gameManager = GameManager.FindInstance();
+                gameManager.ChangeMap(map);
+            }
+        }
 
  
 
