da5f4dbf2659ee6a8836d3094484c09a5819bed0
Josema14 <josemaespin14@gmail.com>
2023-04-14 12:03:23 +0200

    ChangeRoom update

---
+++ Client/Assets/Scripts/Events/CreateRoom.cs
@@ -0,0 +1,35 @@
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CreateRoom : MonoBehaviour
+{
+
+   [SerializeField] private TMP_InputField inputName;
+
+
+    GameManager gameManager;
+  
+
+    public void CreateNewRoom()
+    {
+
+        Debug.Log("Entro");
+        Debug.Log(gameManager);
+        gameManager.StartGame(inputName.text, gameManager.GetAvatarNumber());
+       
+
+    }
+
+
+    public void OpenUI()
+    {
+        this.gameObject.SetActive(true);
+        gameManager = GameManager.FindInstance();
+        GameObject player = gameManager.GetCurrentPlayer();
+        player.GetComponent<CharacterInputHandler>().DeactivateALL();
+
+    }
+}
