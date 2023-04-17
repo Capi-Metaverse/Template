ca412808baa4587edc379fcaf9e5bfd16f4c07e6
pedrokaskiscapgeminis <107995959+pedrokaskiscapgeminis@users.noreply.github.com>
2023-04-17 14:22:41 +0200

    push getFriendList

--- Client/Assets/Scripts/Friends/AddFriendManager.cs
+++ Client/Assets/Scripts/Friends/AddFriendManager.cs
@@ -32,4 +32,33 @@ public class AddFriendManager : MonoBehaviour
         Debug.LogError("Failed to add friend: " + error.ErrorMessage);
         // Handle failure, if necessary
     }
+    public void GetFriendsList()
+    {
+        ExecuteCloudScriptRequest request = new ExecuteCloudScriptRequest
+        {
+            FunctionName = "getFriendsList",
+            GeneratePlayStreamEvent = true
+        };
+
+        PlayFabClientAPI.ExecuteCloudScript(request, OnGetFriendsListSuccess, OnGetFriendsListFailure);
+    }
+
+    // Callback for successful CloudScript function call
+    private void OnGetFriendsListSuccess(ExecuteCloudScriptResult result)
+    {
+     
+        if (result.FunctionResult != null)
+        {
+
+            Debug.Log(result.FunctionResult);
+            // Access the Friends List from 'result.FunctionResult["Friends"]'
+
+        }
+    }
+
+    // Callback for failed CloudScript function call
+    private void OnGetFriendsListFailure(PlayFabError error)
+    {
+        Debug.LogError("Failed to retrieve Friends List: " + error.ErrorMessage);
+    }
 }
