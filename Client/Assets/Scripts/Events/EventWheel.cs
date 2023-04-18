05c4196ddeb3565b8a568e68576c7dad3666a544
antaguil <antonio-manuel.aguilera-ruiz.external@capgemini.com>
2023-04-18 13:14:19 +0200

    Changed Animation Trigger
    The animations trigger has been changed by an int to make it more scalable. Also some animations have been added

--- Client/Assets/Scripts/Events/EventWheel.cs
+++ Client/Assets/Scripts/Events/EventWheel.cs
@@ -3,36 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AnimationList
+{
+    None,
+    Clapping,
+    Waving,
+    Capoeira,
+    Salute,
+    Defeated,
+    TwistedDance
+}
 public class EventWheel : NetworkTransform
 {
     public Animator animator;
 
-    string animationToPlay = "";
+    AnimationList animationToPlay = AnimationList.None;
     
     public void setAnimation1()
     {
-        animationToPlay = "Clapping";
+        animationToPlay = AnimationList.Clapping;
         Debug.Log(animationToPlay);
     }
 
     public void setAnimation2()
     {
-        animationToPlay = "Waving";
+        animationToPlay = AnimationList.Waving;
         Debug.Log(animationToPlay);
     }
 
     public void setAnimation3()
     {
-        animationToPlay = "Capoeira";
+        animationToPlay = AnimationList.Capoeira;
         Debug.Log(animationToPlay);
     }
 
+    public void setAnimation4()
+    {
+        animationToPlay = AnimationList.Salute;
+        Debug.Log(animationToPlay);
+    }
+
+    public void setAnimation5()
+    {
+        animationToPlay = AnimationList.Defeated;
+        Debug.Log(animationToPlay);
+    }
+
+    public void setAnimation6()
+    {
+        animationToPlay = AnimationList.TwistedDance;
+        Debug.Log(animationToPlay);
+    }
+
+
     public override void Render()
     {
-        if (animationToPlay != "") { 
+        if (animationToPlay != AnimationList.None) { 
             if (animator == null) { animator = this.gameObject.transform.parent.GetComponentInChildren<Animator>(); };
-            animator.SetTrigger(animationToPlay);
-            animationToPlay = "";
+            animator.SetInteger("AnimationWheel",(int)animationToPlay);
+
+            //Set the value to zero to end animation
+            animationToPlay = AnimationList.None;
+        }
+        else
+        {
+            animator.SetInteger("AnimationWheel", (int)animationToPlay);
         }
     }
 }
