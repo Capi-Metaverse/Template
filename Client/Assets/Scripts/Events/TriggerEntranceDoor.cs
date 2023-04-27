b2248d106b0ef4b8a4b4db293849370a5df58553
Josema14 <josemaespin14@gmail.com>
2023-04-27 10:41:45 +0200

    trigger door fixed??

--- Client/Assets/Scripts/Events/TriggerEntranceDoor.cs
+++ Client/Assets/Scripts/Events/TriggerEntranceDoor.cs
@@ -4,9 +4,10 @@ public class TriggerEntranceDoor : MonoBehaviour
 {
     [SerializeField] private Animator EntranceDoor = null;
 
-    public bool MemberInside { get; set; } = false;
+    public bool MemberInside = false;
 
     float startTime = 0.0f;
+
 
     private void OnTriggerStay(Collider other)
     {
@@ -14,17 +15,27 @@ public class TriggerEntranceDoor : MonoBehaviour
         {
             EntranceDoor.Play("OfficeEntranceGlassDoor", 0, 0.0f);
         }
-
         MemberInside = true;
+       
 
-        startTime = 5;
-        while (startTime>0)
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        MemberInside = false;
+
+        startTime = 5.0f;
+        while (startTime > 0)
         {
+
             startTime -= Time.deltaTime;
         }
 
-        MemberInside = false;
-        EntranceDoor.Play("OfficeEntranceGlassDoorInverse", 0, 0.0f);
+        if(MemberInside == false)
+        {
+            EntranceDoor.Play("OfficeEntranceGlassDoorInverse", 0, 0.0f);
+        }
+
 
     }
 
