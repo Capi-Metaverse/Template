c45eb2e44c17d9435005558f2f2bdc41a357f12a
antaguil <antonio-manuel.aguilera-ruiz.external@capgemini.com>
2023-04-27 10:05:06 +0200

    Modified Glass Entrance 2

--- Client/Assets/Scripts/Events/TriggerEntranceDoor.cs
+++ Client/Assets/Scripts/Events/TriggerEntranceDoor.cs
@@ -1,43 +1,31 @@
-using ExitGames.Client.Photon.StructWrapping;
-using Fusion;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerEntranceDoor : NetworkBehaviour
+public class TriggerEntranceDoor : MonoBehaviour
 {
     [SerializeField] private Animator EntranceDoor = null;
 
-    [Networked(OnChanged = nameof(OnChangedMemberInside))]
     public bool MemberInside { get; set; } = false;
 
     float startTime = 0.0f;
 
     private void OnTriggerStay(Collider other)
     {
-        
-        MemberInside = true;
-        startTime = 5;
+        if (MemberInside == false)
+        {
+            EntranceDoor.Play("OfficeEntranceGlassDoor", 0, 0.0f);
+        }
 
-        
+        MemberInside = true;
+
+        startTime = 5;
         while (startTime>0)
         {
             startTime -= Time.deltaTime;
         }
+
         MemberInside = false;
+        EntranceDoor.Play("OfficeEntranceGlassDoorInverse", 0, 0.0f);
 
-    }
-
-    private void OnChangedMemberInside()
-    {
-        if (MemberInside)
-        {
-            EntranceDoor.Play("OfficeEntranceGlassDoor", 0, 0.0f);
-        }
-        else
-        {
-            EntranceDoor.Play("OfficeEntranceGlassDoorInverse", 0, 0.0f);
-        }
     }
 
 }
