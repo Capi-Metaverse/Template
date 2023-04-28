79f4f9a93319cfeb81cff80832aead639529a83a
pedrokaskiscapgeminis <107995959+pedrokaskiscapgeminis@users.noreply.github.com>
2023-04-28 10:37:23 +0200

    push paint

---
+++ Client/Assets/Scripts/Lineas/DrawLinesOnPlane.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLinesOnPlane : MonoBehaviour
+{
+    public Material lineMaterial;
+    private float lineWidth = 0.1f;
+    public LayerMask planeLayer;
+    private List<Vector3> linePoints = new List<Vector3>();
+    private LineRenderer lineRenderer;
+    public Camera Camera;
+
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = lineMaterial;
+        lineRenderer.widthMultiplier = lineWidth ;
+        lineRenderer.positionCount = 0;
+        lineRenderer.useWorldSpace = true;
+    }
+ 
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            
+            if (Physics.Raycast(Camera.ScreenPointToRay(Input.mousePosition), out hit, float.PositiveInfinity, planeLayer) && hit.transform.tag == "Paint")
+            {
+                linePoints.Add(hit.point);
+            }
+        }
+
+        lineRenderer.positionCount = linePoints.Count;
+        lineRenderer.widthMultiplier = lineWidth;
+        lineRenderer.SetPositions(linePoints.ToArray());
+
+    }
+}
