4e724335060c7e49ec076b26268b90c14d9106b3
QuantumBBoy <119490912+QuantumBBoy@users.noreply.github.com>
2023-05-16 12:39:24 +0200

    Retrieving UICard working

--- Client/Assets/Scripts/Player/UserCardUI/PlayerInteractionE.cs
+++ Client/Assets/Scripts/Player/UserCardUI/PlayerInteractionE.cs
@@ -1,11 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PlayFab;
 using PlayFab.ClientModels;
-using System;
-using Newtonsoft.Json;
 using static Fusion.NetworkCharacterController;
+using PlayFab;
 
 public class PlayerInteractionE : MonoBehaviour , IMetaEvent
 {
@@ -15,41 +12,22 @@ public class PlayerInteractionE : MonoBehaviour , IMetaEvent
     GameObject _eventObject;
     GameObject IMetaEvent.eventObject { get => _eventObject; set => _eventObject = value; }
 
-    void HandleReadOnlyDataCallback(string value)
-    {
-        if (value != null)
-        {
-            // Do something with the retrieved value
-            Debug.Log("Received value: " + value);
-        }
-        else
-        {
-            Debug.Log("Value not found or error occurred.");
-        }
-    }
-
     public void activate(bool host)
     {
         string playfabid =_eventObject.GetComponent<NetworkPlayer>().playfabIdentity;
         Debug.Log("PlayfabID del pulsado: " + playfabid);
         //get UIcard
-        GetPublicDataFromOtherPlayer("76CEE0B9316C54B6", "userUICard", HandleReadOnlyDataCallback);
+        GetPublicDataFromOtherPlayer("B8F668536256F472", "userUICard");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        gameManager = GameManager.FindInstance();
+        //gameManager = GameManager.FindInstance();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void GetPublicDataFromOtherPlayer(string otherPlayerId, string key, Action<string> callback)
+    public void GetPublicDataFromOtherPlayer(string otherPlayerId, string key)
     {
         var request = new GetUserDataRequest
         {
@@ -57,22 +35,19 @@ public class PlayerInteractionE : MonoBehaviour , IMetaEvent
             Keys = new List<string> { key }
         };
 
-        PlayFabClientAPI.GetUserReadOnlyData(request, result =>
-        {
-            if (result.Data.TryGetValue(key, out var value))
-            {
-                // Do something with the retrieved value
-                callback?.Invoke(value.Value);
-                Debug.Log($"Retrieved value {value.Value} for key {key} from player {otherPlayerId}");
-            }
-            else
-            {
-                callback?.Invoke(null);
-                Debug.Log($"Could not retrieve value for key {key} from player {otherPlayerId}");
-            }
-        }, error =>
-        {
-            Debug.LogError($"Error retrieving read-only data for player {otherPlayerId}: {error.ErrorMessage}");
-        });
+        PlayFabClientAPI.GetUserData(request, OnUpdateUserDataSuccess, OnUpdateUserDataFailure);
+       
     }
+
+    private void OnUpdateUserDataSuccess(GetUserDataResult result)
+    {
+        Debug.Log("Rsult to user successfully!");
+        Debug.Log(result.Data["userUICard"].Value);
+    }
+
+    private void OnUpdateUserDataFailure(PlayFabError error)
+    {
+        Debug.LogError("Failed to upload image to user: " + error.GenerateErrorReport());
+    }
+
 }
