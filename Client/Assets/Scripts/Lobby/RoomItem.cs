848d35cc77f75c1dbb130bea87eabd33baaa1805
slopezpu <130362760+slopezpu@users.noreply.github.com>
2023-05-25 13:33:50 +0200

    Comment RoomItem NavigationTab

--- Client/Assets/Scripts/Lobby/RoomItem.cs
+++ Client/Assets/Scripts/Lobby/RoomItem.cs
@@ -1,8 +1,12 @@
-
 using UnityEngine;
 using TMPro;
 using Fusion;
 
+
+/// <summary>
+/// This cript is attached to the RoomItem prefab that appears in the lists of rooms to join in the Lobby scene
+/// and when the button is clicked the user enter into the room
+/// </summary>
 public class RoomItem : MonoBehaviour
 {
     /*---------------------VARIABLES-----------------*/
@@ -16,17 +20,25 @@ public class RoomItem : MonoBehaviour
     {
         _lobbyManager = FindObjectOfType<LobbyManager>();//We get the LobbyManager from scene
     }
-    public void SetRoomName(string _roomName)
-    //Set name of the room
-    {
-        roomName.text = _roomName;
-    }
+    //public void SetRoomName(string _roomName)
+    ////Set name of the room
+    //{
+    //    roomName.text = _roomName;
+    //}
+
+    /// <summary>
+    /// Join a determinated room when pressed the room
+    /// </summary>
     public void OnClickItem()
     //Enter into LobbyRoom
     {
         _lobbyManager.OnClickJoinSession(sessionInfo);
     }
 
+    /// <summary>
+    /// Sets the info of the RoomItem. Used by LobbyManager
+    /// </summary>
+    /// <param name="sessionInfo"></param>
     public void SetSessionInfo(SessionInfo sessionInfo)
     {
         this.sessionInfo = sessionInfo;
--- Client/Assets/Scripts/Login/TabNavigation.cs
+++ Client/Assets/Scripts/Login/TabNavigation.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Used for navigate in the UI by pressing the tab
+/// </summary>
 public class TabNavigation : MonoBehaviour
 {
+    //Put in the inspector all the components to navigate
     public GameObject[] navigationObjects;
 
     private int currentIndex = 0;
 
+    //Select the first item of the array
     private void Start()
     {
         EventSystem.current.SetSelectedGameObject(navigationObjects[currentIndex]);
     }
+
+    //Go to the next item of the array when tab is pressed
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
