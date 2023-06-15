6dabe7538f5522af5d95e487264882e3c4195b32
antaguil <antonio-manuel.aguilera-ruiz.external@capgemini.com>
2023-06-15 13:51:00 +0200

    Minor Modifications

--- Client/Assets/Module_Metaverse/Scripts/Events/CreateRoomPanel.cs
+++ Client/Assets/Module_Metaverse/Scripts/Events/CreateRoomPanel.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateRoomPanel : MonoBehaviour, IMetaEvent
@@ -7,17 +5,10 @@ public class CreateRoomPanel : MonoBehaviour, IMetaEvent
     [SerializeField] private CreateRoom createScript;
     GameObject _eventObject;
     GameObject IMetaEvent.eventObject { get => _eventObject; set => _eventObject = value; }
-    private GameManager gameManager;
 
     public void activate(bool host)
     {
-        //We find the game manager
-        gameManager = GameManager.FindInstance();
         //Open UI
-
         createScript.OpenUI();
     }
-
-
-
 }
