b5770fa018d2460d2c6e942eee27c6d1d18ef6d0
slopezpu <130362760+slopezpu@users.noreply.github.com>
2023-06-26 11:48:27 +0200

    AchievementReached

---
+++ Client/Assets/Module_Metaverse/Scripts/Login/PlayFab/Player/Achievements/AchievementReached.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Entities;
+using UnityEditor;
+using UnityEngine;
+
+public class AchievementReached : MonoBehaviour
+{
+    public static void ShowAchievement(string achName, Transform achZone)
+    {
+        string path = "Assets/Module_Metaverse/Prefabs/Achievement/Achievement.prefab";
+        GameObject achPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+
+        Transform child = achPrefab.transform.GetChild(0);
+        string text = child.GetComponent<TMP_Text>().text;
+        text = achName;
+
+        Instantiate(achPrefab, achZone);
+    }
+}
